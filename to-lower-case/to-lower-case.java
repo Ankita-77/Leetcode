8d433adf7003293b1f644ecacdbced34fe3e71b1
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-08-11 19:42:01 +0530

    Time: 0 ms (100.00%), Space: 37.2 MB (57.29%) - LeetHub

--- to-lower-case/to-lower-case.java
+++ to-lower-case/to-lower-case.java
@@ -1,11 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder(s); //stringBuilder it is more efficient
         for(int i=0;i<sb.length();i++){
-            char ch = sb.charAt(i);
-            if(ch >= 'A' && ch <= 'Z'){
-               char lowerCase = (char)('a' + ch - 'A');
-                sb.setCharAt(i,lowerCase);
+            char ch = sb.charAt(i);  // find every character in String s
+            if(ch >= 'A' && ch <= 'Z'){    // if found any uppercase letter
+               char lowerCase = (char)('a' + ch - 'A');    
+                // change this to lowercase let ex H ascii value is 72 - ascii value of A                    is 65 so add this diff in a.
+                sb.setCharAt(i,lowerCase);      // set char at that position
             }
         }
         return sb.toString();
