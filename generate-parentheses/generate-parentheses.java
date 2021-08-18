e34d4fd7b354c977d00fc5eeb2203e9331cb318d
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-08-18 20:28:12 +0530

    Time: 1 ms (72.78%), Space: 39.1 MB (77.35%) - LeetHub

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -1,25 +1,23 @@
 class Solution {
-   public List<String> generateParenthesis(int n) {
+    public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList();
-        backtrack(ans, new StringBuilder(), 0, 0, n);
+        backtrack(ans, "", 0, 0, n);
         return ans;
     }
-
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+    
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
         if (cur.length() == max * 2) {
-            ans.add(cur.toString());
+            ans.add(cur);
             return;
         }
 
         if (open < max) {
-            cur.append("(");
-            backtrack(ans, cur, open+1, close, max);
-            cur.deleteCharAt(cur.length() - 1);
+            backtrack(ans, cur+"(", open+1, close, max);
         }
-        if (close < open) {
-            cur.append(")");
-            backtrack(ans, cur, open, close+1, max);
-            cur.deleteCharAt(cur.length() - 1);
+        if (open > close) {
+            backtrack(ans, cur+")", open, close+1, max);
         }
     }
 }
+
+//The idea here is to only add '(' and ')' that we know will guarantee us a solution (instead of adding 1 too many close). Once we add a '(' we will then discard it and try a ')' which can only close a valid '('. Each of these steps are recursively called.
