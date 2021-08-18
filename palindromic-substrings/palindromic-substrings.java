dbc2a1a7a0647f2cc73f32b21fff78a72f5b25e5
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-08-18 13:00:41 +0530

    Update palindromic-substrings.java

--- palindromic-substrings/palindromic-substrings.java
+++ palindromic-substrings/palindromic-substrings.java
@@ -3,16 +3,19 @@ class Solution {
     public int countSubstrings(String s) {
         for(int i=0;i<s.length();i++){
             isPalindrome(s,i,i);
+            //To check the palindrome of odd length palindromic sub-string
             isPalindrome(s,i,i+1);
+            //To check the palindrome of even length palindromic sub-string
         }
         return count;
     }
     
     public void isPalindrome(String s, int i, int j){
-        while(i>=0 && j < s.length() && s.charAt(i) == s.charAt(j)){
-            count++;
-            i--;
-            j++;
-        }
+       while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){    
+           //Check for the palindrome string 
+            count++;    //Increment the count if palindromic substring found
+            i--;        //To trace string in left direction
+            j++;        //To trace string in right direction
+       }
     }
 }
