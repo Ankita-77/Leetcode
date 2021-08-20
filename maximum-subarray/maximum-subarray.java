ee4caefa92c929e688b24d54cbf23a31bdb49cce
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-08-20 23:42:55 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (28.13%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -1,12 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0;
-        int maxi = nums[0];
-        for(int i=0;i<nums.length;i++){
-            sum = sum + nums[i];
+        int maxi = nums[0]; 
+        
+        for(int i=0;i<nums.length;i++){    
+            sum = sum + nums[i];  
+            
+ // if sum is less than 0 then sum=0 and if sum > maxi then maxi = sum.         
             if(sum > maxi) maxi = sum;
             if(sum < 0) sum = 0;;
         }
         return maxi;
     }
 }
+
+
+// kadane's algorithm
+// Time Complexity = O(N) 
+// Space Complexity = O(1);
