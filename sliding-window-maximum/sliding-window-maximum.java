08d6487ed21eba7d500d96f4b9192888159e4d86
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-09-13 23:33:29 +0530

    Time: 31 ms (57.03%), Space: 54 MB (75.66%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,42 +1,25 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int nge[] = new int[arr.length];
-        int ans[] = new int[arr.length - k + 1];
-    
-        Stack<Integer> st = new Stack<>();
-
-        st.push(arr.length - 1);
-        nge[arr.length - 1] = arr.length;
-
-        for(int i=arr.length-2; i>=0; i--){
-            // pop ans push
-
-            while(st.size() > 0 && arr[i] >= arr[st.peek()]){
-                st.pop();
-            }
-
-            if(st.size() == 0){
-                nge[i] = arr.length;
-            }else{
-                nge[i] = st.peek();
-            }
-
-            st.push(i);
-        }
-
-        int j = 0;
-        for(int i=0; i <= arr.length - k; i++){
-            if(j < i){
-                j = i;
-            }
-
-            while(nge[j] < i + k){
-                j = nge[j];
-            }
-
-            ans[i] = arr[j];
-        }
-        
-        return ans;
+    public int[] maxSlidingWindow(int[] a, int k) {
+        int n = a.length;
+		int[] r = new int[n-k+1];
+		int ri = 0;
+		// store index
+		Deque<Integer> q = new ArrayDeque<>();
+		for (int i = 0; i < a.length; i++) {
+			// remove numbers out of range k
+			if (!q.isEmpty() && q.peek() == i - k) {
+				q.poll();
+			}
+			// remove smaller numbers in k range as they are useless
+			while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+				q.pollLast();
+			}
+            
+			q.offer(i);
+			if (i >= k - 1) {
+				r[ri++] = a[q.peek()];
+			}
+		}
+		return r;
     }
 }
