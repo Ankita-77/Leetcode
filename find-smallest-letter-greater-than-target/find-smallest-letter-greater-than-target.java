5d7590e1f8deed9b4cbde10286d827b851e72f31
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-09-27 22:06:34 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (46.92%) - LeetHub

---
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,26 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length-1;
+        
+// return the first element if there is no character greater than target in the array
+        if(target >= letters[letters.length-1]){
+            return letters[0];
+        }
+        
+        while(start <= end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(target >= letters[mid]){
+                start = mid+1;
+            }
+            else if(target < letters[mid]){
+                end = mid-1;
+            }
+        }
+        
+// after the loop breaks; start element will be smallest element greater than target
+        return letters[start];
+    }
+}
