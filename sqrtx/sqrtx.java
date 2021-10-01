88a44fb2514dcce472363a5eabd9edb704ad3a44
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-10-01 12:36:03 +0530

    Time: 2 ms (42.04%), Space: 38.3 MB (16.44%) - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x < 2){
+            return (int)x;
+        }
+        
+        long low = 1;
+        long high = x;
+        
+        while(low <= high){
+            long mid = low + (high - low) / 2;
+            
+            if(mid * mid == x){
+                return (int)mid;
+            }else if(mid * mid > x){
+                high = mid - 1;
+            }else if(mid * mid < x){
+                low = mid + 1;
+            }
+        }
+        
+        return (int)high;
+    }
+}
