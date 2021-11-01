eec01a291a8a9add6ef2207db15668dbad927168
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-11-02 00:10:22 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (45.51%) - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,20 +15,50 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        
+        // ITERATIVE
         List<Integer> preorder = new ArrayList<Integer>();
-        dfs(root, preorder);
+        if(root == null) return preorder;
+        
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        
+        while(!st.isEmpty()){
+            root = st.pop();
+            preorder.add(root.val);
+            
+            if(root.right != null){
+                st.push(root.right);
+            }
+            
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        
         return preorder;
     }
     
-    private void dfs(TreeNode node, List<Integer> preorder){
-        if(node == null){
-            return;
-        }
+    
+    
+    
+    
+//  RECURSIVE
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> preorder = new ArrayList<Integer>();
+//         dfs(root, preorder);
+//         return preorder;
+//     }
+    
+//     private void dfs(TreeNode node, List<Integer> preorder){
+//         if(node == null){
+//             return;
+//         }
         
-        preorder.add(node.val);
-        dfs(node.left, preorder);
-        dfs(node.right, preorder);
-    }
+//         preorder.add(node.val);
+//         dfs(node.left, preorder);
+//         dfs(node.right, preorder);
+//     }
 }
 
 // time complexity = O(N)
