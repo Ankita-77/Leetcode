a947ac0ec8d46a923d828c18c86d0d2d5214c74d
Ankita-77 <63393185+Ankita-77@users.noreply.github.com>
2021-11-03 01:29:07 +0530

    Time: 0 ms (100.00%), Space: 37.4 MB (62.69%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,18 +15,38 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> postorder = new ArrayList<>();
-        dfs(root, postorder);
-        return postorder;
+        
+        // ITERATIVE
+        LinkedList<Integer> result = new LinkedList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                result.addFirst(p.val);  // Reverse the process of preorder
+                p = p.right;             // Reverse the process of preorder
+            } else {
+                TreeNode node = stack.pop();
+                p = node.left;           // Reverse the process of preorder
+            }
+        }
+        return result;
     }
     
-    private void dfs(TreeNode node, List<Integer> postorder){
-        if(node == null){
-            return;
-        }
+    // RECURSIVE
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> postorder = new ArrayList<>();
+//         dfs(root, postorder);
+//         return postorder;
+//     }
+    
+//     private void dfs(TreeNode node, List<Integer> postorder){
+//         if(node == null){
+//             return;
+//         }
         
-        dfs(node.left, postorder);
-        dfs(node.right, postorder);
-        postorder.add(node.val);
-    }
+//         dfs(node.left, postorder);
+//         dfs(node.right, postorder);
+//         postorder.add(node.val);
+//     }
 }
