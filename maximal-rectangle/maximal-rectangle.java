618ec77f1ab34499cd933b743d45f4d0044817b7
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2021-11-30 22:51:25 +0530

    Time: 11 ms (45.28%), Space: 40.9 MB (99.07%) - LeetHub

--- maximal-rectangle/maximal-rectangle.java
+++ maximal-rectangle/maximal-rectangle.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        
-        if (matrix==null||matrix.length==0||matrix[0].length==0)
+        if(matrix.length == 0 || matrix == null || matrix[0].length == 0){
             return 0;
+        }
         
         int n = matrix.length;
         int m = matrix[0].length;
+        int maxHist = Integer.MIN_VALUE;
         
-        int[] h = new int[m];
-        int maxHistArea = Integer.MIN_VALUE;
-        
+        int h[] = new int[m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                
                 if(matrix[i][j] != '0'){
                     h[j] += 1;
                 }else{
@@ -20,29 +18,25 @@ class Solution {
                 }
             }
             
-            int currHistArea = MAH(h);
-            if(currHistArea > maxHistArea){
-                 maxHistArea = currHistArea;
+            int currHist = MAH(h);
+            if(currHist > maxHist){
+                maxHist = currHist;
             }
         }
-        
-         return maxHistArea;
-    
+        return maxHist;
     }
     
-    // find maximum area of histogram
-     public int MAH(int[] hist){
-        
-        int n = hist.length;
+    public int MAH(int[] arr){
+        int n = arr.length;
         
         int nsl[] = new int[n];
         int nsr[] = new int[n];
         
         Stack<Integer> st = new Stack<>();
         
-        // find nearest smaller left
-        for(int i=0; i<n; i++){
-            while(st.size() > 0 &&  hist[i] <= hist[st.peek()]){
+        // nearest smaller to left
+        for(int i=0; i<arr.length; i++){
+            while(st.size() > 0 && arr[st.peek()] >= arr[i]){
                 st.pop();
             }
             
@@ -56,15 +50,15 @@ class Solution {
         }
         
         
-        // find nearest smaller right
+        // nearest smaller to right
         st = new Stack<>();
-        for(int i=n-1; i>=0; i--){
-            while(st.size() > 0 &&  hist[i] <= hist[st.peek()]){
+        for(int i=arr.length-1; i>=0; i--){
+            while(st.size() > 0 && arr[st.peek()] >= arr[i]){
                 st.pop();
             }
             
             if(st.size() == 0){
-                nsr[i] = n;
+                nsr[i] =  n;
             }else{
                 nsr[i] = st.peek();
             }
@@ -72,15 +66,15 @@ class Solution {
             st.push(i);
         }
         
-        
-       int ans[] = new int[n];
-       int area = 0;
-       for(int i=0; i<n; i++){
-            ans[i] = (Math.abs(nsr[i] - nsl[i]) - 1) * hist[i];
+        int ans[] = new int[n];
+        int area = 0;
+        for(int i=0; i<n; i++){
+            ans[i] = (Math.abs(nsr[i] - nsl[i] -1)) * arr[i];
             if(area < ans[i]){
                 area = ans[i];
             }
-       }
-       return area;
+        }
+        
+        return area;
     }
 }
