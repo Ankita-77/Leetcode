a4cd6a127df37b9518f9be71a96f5791a520a554
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2021-12-05 01:38:27 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (98.91%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.java
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -15,17 +15,17 @@ public class Solution {
         
         ListNode slow = head;
         ListNode fast = head;
-        ListNode entry = head;
         
-        
-        while(fast != null && fast.next != null){
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
             
-            if(slow == fast){  // there is a cycle
-                while(slow != entry){       // found entry location
-                    slow = slow.next;
+            if(slow == fast){
+                ListNode entry = head;
+                
+                while(entry != slow){
                     entry = entry.next;
+                    slow = slow.next;
                 }
                 
                 return entry;
