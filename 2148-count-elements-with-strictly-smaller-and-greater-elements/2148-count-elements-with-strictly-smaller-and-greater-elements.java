a7d0d86ae5206b7a01ac367b1eb676ddcfbea7b0
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2022-01-23 15:24:00 +0530

    Time: 1 ms (50.00%), Space: 39.4 MB (12.50%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int ans = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        // Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(min > nums[i]) min = nums[i];
+            if(max < nums[i]) max = nums[i];
+        }
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > min && nums[i] < max){
+                ans++;
+            }
+        }
+        return ans;
+        
+        // by using sorting O(nlogn) + O(n)
+        // no need to do sorting just find min and max
+    }
+}
