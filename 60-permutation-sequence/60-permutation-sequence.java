d3f04d5682ef6bc63c4aeae747044230e7bdbcbe
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2022-01-26 01:27:51 +0530

    Time: 5 ms (33.70%), Space: 38.4 MB (43.22%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        int fact = 1;
+        ArrayList<Integer> num = new ArrayList<>();
+        for(int i=1; i<n; i++){
+            fact = fact * i;
+            num.add(i);
+        }
+        
+        num.add(n);
+        k = k - 1;
+        String ans = "";
+        
+        while(true){
+            ans += "" + num.get(k/fact);
+            num.remove(k/fact);
+            
+            if(num.size() == 0){
+                break;
+            }
+            
+            k = k % fact;
+            fact = fact / num.size();
+        }
+        
+        return ans;
+    }
+}
+
+// Time Complexity: O(N) * O(N) = O(N^2)
+
+// Reason: We are placing N numbers in N positions. This will take O(N) time. For every number, we are reducing the search space by removing the element already placed in the previous step. This takes another O(N) time.
