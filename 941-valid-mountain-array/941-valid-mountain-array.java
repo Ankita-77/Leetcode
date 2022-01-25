5a7967a996d0dc17ba59250ffe0b67a774ca33a1
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2022-01-25 18:56:48 +0530

    Time: 2 ms (56.28%), Space: 51.4 MB (39.75%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -1,42 +1,57 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if(arr.length < 3){
+        
+        if(arr.length < 3) return false;
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        
+        while(i+1 < n && arr[i] < arr[i+1]) i++;
+        while(j > 0 && arr[j] < arr[j-1]) j--;
+        
+        if(i > 0 && i == j && j < n-1)
+            return true;
+        else
             return false;
-        }
-        int val = findMax(arr);
+    }     
         
-        if(val == arr.length-1){
-            return false;
-        }
+//         if(arr.length < 3){
+//             return false;
+//         }
+//         int val = findMax(arr);
         
-        if(val == 0){
-            return false;
-        }
+//         if(val == arr.length-1){
+//             return false;
+//         }
         
-        for(int i=val; i>=1; i--){
-            if(!(arr[i] > arr[i-1])){
-                return false;
-            }
-        }
+//         if(val == 0){
+//             return false;
+//         }
         
-        for(int i=val; i<arr.length-1; i++){
-            if(!(arr[i] > arr[i+1])){
-                return false;
-            }
-        }
-        return true;
-    }
+//         for(int i=val; i>=1; i--){
+//             if(!(arr[i] > arr[i-1])){
+//                 return false;
+//             }
+//         }
+        
+//         for(int i=val; i<arr.length-1; i++){
+//             if(!(arr[i] > arr[i+1])){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
     
-    public int findMax(int[] arr){
-        int max = arr[0];
-        int point = 0;
-        for(int i=1; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                point = i;
-            }
-        }
+//     public int findMax(int[] arr){
+//         int max = arr[0];
+//         int point = 0;
+//         for(int i=1; i<arr.length; i++){
+//             if(arr[i] > max){
+//                 max = arr[i];
+//                 point = i;
+//             }
+//         }
         
-        return point;
-    }
+//         return point;
+//     }
 }
