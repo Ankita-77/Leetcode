e1b49759867e8d8c788df11ebced2a532fe73bee
Ankita Kumari <63393185+Ankita-77@users.noreply.github.com>
2022-02-22 19:46:55 +0530

    Time: 61 ms (74.18%), Space: 68.9 MB (29.34%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        List<Engineer> engineers = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++)
+            engineers.add(new Engineer(speed[i], efficiency[i]));
+        
+        engineers.sort((a, b) -> b.efficiency - a.efficiency);
+        
+        //will keep slowest members at the top, fastest ones stay in the team longest
+        PriorityQueue<Engineer> currentTeam = new PriorityQueue<>((a, b) -> a.speed - b.speed);
+        
+        long teamSpeed = 0;
+        long maxPerformance = Long.MIN_VALUE;
+        
+        for(Engineer newHire : engineers){
+            if(currentTeam.size() == k){
+                Engineer slowGuy = currentTeam.poll();
+                teamSpeed -= slowGuy.speed;
+            }
+            
+            currentTeam.add(newHire);
+            teamSpeed += newHire.speed;
+            long performanceWithNewGuy = teamSpeed * (long) newHire.efficiency;
+            maxPerformance = Math.max(maxPerformance, performanceWithNewGuy);
+
+        }
+        return (int) (maxPerformance % MOD);
+  }
+    
+    int MOD = (int) (1e9 + 7);
+    
+    class Engineer{
+        int speed;
+        int efficiency;
+        
+        public Engineer(int speed, int efficiency){  
+            this.speed = speed;
+            this.efficiency = efficiency;
+        }
+    }
+}
